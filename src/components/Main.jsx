cf230fbffeeab8b1665c31392a56fd12ef23085c
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2024-08-16 18:29:41 +0300

    About Us and Visit Us sections finished, minor improvements of other sections

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import cafeInside from "../assets/cafe-inside.jpg";
+import aboutUs from "../assets/about.jpeg";
 
 const Main = () => {
   return (
@@ -7,7 +8,7 @@ const Main = () => {
       <section className="intro">
         <div className="intro__wrap">
           <img src={cafeInside} alt="cafe" className="intro__image" />
-          <div className="intro__overlay"></div>
+          <div className="overlay"></div>
           <div className="intro__content">
             <h1 className="intro__title">Refresh You Daily</h1>
             <p className="intro__text">
@@ -17,7 +18,44 @@ const Main = () => {
               warm atmosphere and exceptional service that will make you feel
               right at home.
             </p>
-            <a href="" className="intro__button">Visit Us</a>
+            <a href="" className="intro__button button">
+              Visit Us
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section className="about">
+        <div className="wrapper">
+          <div className="about__wrap">
+            <div className="about__content">
+              <h2 className="about__title">About us</h2>
+              <p className="about__text">
+                We are a cafe located in London, dedicated to offering a
+                friendly and welcoming atmosphere. Our team is passionate about
+                serving quality coffee and freshly made pastries.
+              </p>
+              <p className="about__text">
+                We believe in bringing people together through great food and
+                memorable experiences. Visit us to enjoy a relaxing time with
+                friends and family.
+              </p>
+            </div>
+            <div className="about__image">
+              <img src={aboutUs} alt="" className="about__image-pic" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="visit-us">
+        <div className="visit-us__wrap">
+          <div className="overlay"></div>
+          <div className="visit-us__content">
+            <h2 className="visit-us__title">Visit Wake Up Cafe</h2>
+            <a href="" className="visit-us__button button">
+              Explore Menu
+            </a>
           </div>
         </div>
       </section>
